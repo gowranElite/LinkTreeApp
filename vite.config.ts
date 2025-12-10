4831fb4fbcbb076848f5bc8ed09c9eeae21d6b91
Goran Nadiri <goran.nadiri@hotmail.com>
2025-12-10 11:41:12 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/LinkTreeApp", // important for GH Pages routing
+  base: "/LinkTreeApp/", // important for GH Pages routing
 });
