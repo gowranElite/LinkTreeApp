429121157cec26d3d32a3b8ea44d4d3b113ecb63
gowran1337 <goran.nadiri@hotmail.com>
2026-02-23 09:10:41 +0100

    switcheds sb account

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://vbxcxdoaxtgaavzvnsim.supabase.co";
-const supabaseAnonKey = "sb_publishable_RbfqyuStObkQFMD-yt-Fpw_U1Jhm8ey";
+const supabaseUrl = "https://irxycqgncaypaeuowjea.supabase.co";
+const supabaseAnonKey = "sb_publishable_LMfT5blGED4rmOvQg9BIRg_UYkVZ_rL";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
